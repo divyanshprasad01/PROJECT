977f5d6098594443fd4a4f803c62b293d9f09c44
Divyansh Prasad <divyanshprasad01@gmail.com>
2025-04-13 17:04:31 +0530

    Initialized the repo

---
+++ Authenticator/src/main/java/com/EduHub/Authenticator/Config/SecurityConfigrations.java
@@ -0,0 +1,29 @@
+package com.EduHub.Authenticator.Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.SessionManagementDsl;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfigrations {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity)
+            throws Exception {
+
+        httpSecurity.csrf(customizer -> customizer.disable());
+        httpSecurity.authorizeHttpRequests(request -> request.anyRequest().authenticated());
+        httpSecurity.httpBasic(Customizer.withDefaults());
+        httpSecurity.formLogin(Customizer.withDefaults());
+        httpSecurity.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        return httpSecurity.build();
+    }
+
+}
---
+++ Authenticator/src/test/java/com/EduHub/Authenticator/AuthenticatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.EduHub.Authenticator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
